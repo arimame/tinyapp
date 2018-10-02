780b6cd6ad3e174794a1a9697bb0d2ff54e663a4
Alissa Balge <alissa.balge@gmail.com>
2018-10-02 19:16:47 +0000

    added get for url database to use in index page

--- express_server.js
+++ express_server.js
@@ -2,10 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.set("view engine", "ejs");
+
+
+var urlDatabase = [
+
+{tinyURL: "b2xVn2", URL: "http://www.lighthouselabs.ca"},
+{tinyURL: "9sm5xK", URL: "http://www.google.com"},
+
+];
 
 app.get("/", (req, res) => {
   res.send("<hmtl><body>hello <b>world</b></body></html>\n");
@@ -14,6 +19,23 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+/*app.get("/urls/:tinyURL", (req, res) => {
+
+  let templateVars = {tinyURL: urlDatabase.url_shows(req.params.tinyURL), URL: urlDatabase.url_shows(req.params.URL)};
+  res.render("urls_shows", templateVars);
+});*/
+
+app.get('/articles/:tinyURL', function(req, res) {
+  res.render('urls_shows', {tinyURL: urlDatabase.url_shows(req.params.tinyURL)});
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
