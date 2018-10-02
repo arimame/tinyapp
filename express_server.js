5be9a40859660fa7421721a8126127d0fa590dc9
Alissa Balge <alissa.balge@gmail.com>
2018-10-02 21:29:49 +0000

    added header template

--- express_server.js
+++ express_server.js
@@ -5,12 +5,18 @@ var PORT = 8080;
 app.set("view engine", "ejs");
 
 
-var urlDatabase = [
+// var urlDatabase = [
 
-{tinyURL: "b2xVn2", URL: "http://www.lighthouselabs.ca"},
-{tinyURL: "9sm5xK", URL: "http://www.google.com"},
+// {tinyURL: "b2xVn2", URL: "http://www.lighthouselabs.ca"},
+// {tinyURL: "9sm5xK", URL: "http://www.google.com"},
 
-];
+// ];
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/", (req, res) => {
   res.send("<hmtl><body>hello <b>world</b></body></html>\n");
@@ -25,15 +31,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-/*app.get("/urls/:tinyURL", (req, res) => {
-
-  let templateVars = {tinyURL: urlDatabase.url_shows(req.params.tinyURL), URL: urlDatabase.url_shows(req.params.URL)};
-  res.render("urls_shows", templateVars);
-});*/
-
-app.get('/articles/:tinyURL', function(req, res) {
-  res.render('urls_shows', {tinyURL: urlDatabase.url_shows(req.params.tinyURL)});
-})
+app.get('/urls/:id', function(req, res) {
+  res.render('urls_shows', {tinyURL: req.params.id, URL: urlDatabase[req.params.id]});
+});
 
 
 
