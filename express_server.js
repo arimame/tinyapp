65bcdb3e41cd75fbf8755a147c9961915c30d7ea
Alissa Balge <alissa.balge@gmail.com>
2018-10-04 17:39:38 +0000

    created login in feature and logout button

--- express_server.js
+++ express_server.js
@@ -51,51 +51,118 @@ app.get("/urls.json", (req, res) => {
 
 //homepage, lists all urls with edit and delete button
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  var userToken = req.cookies.user_id;
+  let templateVars = {urls: urlDatabase, userObject: users[userToken].email}; //need to fix this to .email
+  //console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
-//login form
+//login form post
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username)
-  res.redirect("/urls");
-});
+  //res.cookie("user_id", randomID);
+
+  function findUserEmail () {
+    var found = false;
+     for (var key in users) {
+      if (users[key].email === req.body.email) {
+        found = true;
+        return found;
+      }
+    }
+  };
+
+  function findUserPassword () {
+    var match = false;
+     for (var info in users) {
+      console.log("this the password saved ", users[info].password)
+      console.log("this is my password ", req.body.password)
+      if (users[info].password === req.body.password) {
+        match = true;
+        return match;
+     }
+    }
+
+  };
+    var emailFound = findUserEmail();
+    var passwordFound = findUserPassword();
+
+    if (emailFound === false) {
+      es.status(403);
+      res.send("Email not found");
+    } else {
+      if (passwordFound === false) {
+        res.status(403);
+        res.send("Password does not match email");
+      } else {
+          for (var key in users) {
+            if (users[key].email === req.body.email) {
+              res.cookie("user_id", key)
+              res.redirect("/urls");
+            }
+          }
+        }
+      }
+  });
+
 
 //logout button
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/login");
 });
 
 //registration page render
 app.get("/register", (req, res) => {
-let templateVars = {username: req.cookies["username"]};
+var userToken = req.cookies.user_id;
+let templateVars = {userObject: users[userToken]}; //need to fix this to .email???
   res.render("register", templateVars);
+});
+
+
+//login page render
+app.get("/login", (req, res) => {
+var userToken = req.cookies.user_id;
+let templateVars = {userObject: users[userToken]}; //need to fix this to .email???
+  res.render("login", templateVars);
 });
 
 //registration page submisson
 app.post("/register", (req, res) => {
-  if (req.body.email === undefined || req.body.password === undefined) {
+
+ function duplicateEmail () {
+    var match = false;
+     for (var key in users) {
+      if (users[key].email === req.body.email) {
+        match = true;
+        return match;
+      }
+    }
+  };
+  var match = duplicateEmail();
+  if (req.body.email === "" || req.body.password === "") {
     res.status(400);
-    res.send('Please enter a password');
-} else {
+    res.send('Please enter an email and password');
+  } else if (match === true) {
+      res.status(400);
+      res.send('Email is already register');
+  } else {
     var randomID = generateRandomString();
     users[randomID]= {};
     users[randomID].id = randomID;
     users[randomID].email = req.body.email;
     users[randomID].password = req.body.password;
-//console.log(users);
-res.cookie("user_id", randomID)
-res.redirect("/urls");
-}
-
-
+    console.log(users);
+    res.cookie("user_id", randomID)
+    res.redirect("/urls");
+  }
 });
 
 
 //page that allows a new url to be submitted
 app.get("/urls/new", (req, res) => {
-  let templateVars = {username: req.cookies["username"]};
+  var userToken = req.cookies.user_id;
+  let templateVars = {userObject: users[userToken].email}; //need to fix this to .email
+  console.log("this is new", templateVars);
   res.render("urls_new", templateVars);
 });
 
@@ -131,7 +198,8 @@ res.redirect("/urls");
 
 //displays the tiny url and full url when unique id is entered into
 app.get('/urls/:id', function(req, res) {
-  res.render('urls_shows', {tinyURL: req.params.id, URL: urlDatabase[req.params.id], username: req.cookies["username"]});
+  var userToken = req.cookies.user_id;
+  res.render('urls_shows', {tinyURL: req.params.id, URL: urlDatabase[req.params.id], userObject:users[userToken].email}); //need to fix this to .email
 });
 
 
